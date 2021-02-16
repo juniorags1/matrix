f1f122c84b0b5f0c9d84a25c96682a8a2a821875
Junior <junior@Juniors-Air.box>
2021-02-16 19:11:35 -0300

    Auth by domain bug - teste

--- backend/app/services/auth/authorization.js
+++ backend/app/services/auth/authorization.js
@@ -11,7 +11,7 @@ export function domainAuthorization(allowedDomains) {
     }
 
     const [_, domain] = email.split("@");
-    new Error(`E-mail ${domain}`)
+    return cb(new Error(`E-mail ${domain} is not authorized`));
     return allowedDomainsSet.has(domain);
   };
 }
